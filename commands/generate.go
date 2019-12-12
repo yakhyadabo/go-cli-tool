86d88361a4d58da929bf627057976618b8b45961
Yakhya Dabo <yakhyadabo@gmail.com>
2019-12-12 18:33:07 +0000

    Test the flags

--- commands/generate.go
+++ commands/generate.go
@@ -9,9 +9,7 @@ var generateCmd = &cobra.Command{
 	Use:   "generate",
 	Short: "Generate the code",
 	Long:  `It generates the code that can be later used for deployment.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("This is the generate command!")
-	},
+	Run: generate ,
 }
 
 var component string
@@ -21,8 +19,11 @@ var region string
 func init() {
 	rootCmd.AddCommand(generateCmd)
 
-	generateCmd.Flags().StringVarP(&component,"comp", "c","", "Set the name of the component")
-	generateCmd.Flags().StringVarP(&environment,"env", "e","", "Set the environment of the component")
-	generateCmd.Flags().StringVarP(&region,"reg", "r","", "Set the region where the component will be deployed")
+	generateCmd.Flags().StringVarP(&component, "comp", "c", "", "Set the name of the component")
+	generateCmd.Flags().StringVarP(&environment, "env", "e", "", "Set the environment of the component")
+	generateCmd.Flags().StringVarP(&region, "reg", "r", "", "Set the region where the component will be deployed")
 }
 
+func generate (cmd *cobra.Command, args []string) {
+	fmt.Println("Generate " +  component + "-" + environment + " for deployment in " + region)
+}
